36c8e1e7c834910a21e76bba517b71a57d250ed9
Nieskalany13 <m.kow04@wp.pl>
2022-03-01 11:04:55 +0100

    Algorytm_WLP

---
+++ Algorytm wyznaczania liczb pierwszych/Algorytm wyznaczania liczb pierwszych/Algorytm wyznaczania liczb pierwszych.cpp
@@ -0,0 +1,15 @@
+﻿// Algorytm wyznaczania liczb pierwszych.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int tab[5];
+	for (int i = 0; i < 5; i++)
+	cout << "Ile liczb pierwsych chcesz wyświetlić:";
+	cin >> tab[];
+}
+
